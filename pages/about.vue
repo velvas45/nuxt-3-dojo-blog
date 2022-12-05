1ffd9a67e21f92ad3b2687cfe47fb1f22582d23f
INDODAX <indodax@MACBOOKs-MacBook-Pro.local>
2022-12-05 10:46:01 +0700

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo,
+      perspiciatis placeat saepe quibusdam a impedit illo. Iure, veniam ex!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo,
+      perspiciatis placeat saepe quibusdam a impedit illo. Iure, veniam ex!
+    </p>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("/api/currency/GBP");
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo,
+      perspiciatis placeat saepe quibusdam a impedit illo. Iure, veniam ex!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo,
+      perspiciatis placeat saepe quibusdam a impedit illo. Iure, veniam ex!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
